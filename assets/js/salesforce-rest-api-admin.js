fa20fa1b030237a4a77c57e2f0b97cb89c84c157
Jonathan Stegall <jstegall@minnpost.com>
2016-06-24 15:41:56 -0500

    javascript for getting wordpress and salesforce fields in the selected objects, to create a <tr> with <selects> for creating the fieldmap

--- assets/js/salesforce-rest-api-admin.js
+++ assets/js/salesforce-rest-api-admin.js
@@ -25,13 +25,64 @@ function salesforce_date_fields() {
 	});
 }
 
-$(document).ready(function() {
-	$(document)
-		.ajaxStart(function(){
-		    $('.spinner').addClass('is-active');
-		})
-		.ajaxStop(function(){
-		    $('.spinner').removeClass('is-active');
+function add_field_mapping_row() {
+	$('#add-field-mapping').click(function() {
+		var salesforce_object = $('#salesforce_object').val();
+		var wordpress_object = $('#wordpress_object').val();
+		//if ($('table.fields tbody tr').length > 0) {
+		//	console.log('there is already a row. clone it');
+		//} else {
+		if (wordpress_object !== '' && salesforce_object !== '') {
+			var row_count = $('table.fields tbody tr').length;				
+			fieldmap_fields(wordpress_object, salesforce_object, row_count);
+		} else {
+			$(this).parent().append('<span class="missing-object">You have to pick a WordPress object and a Salesforce object to add field mapping.');
+		}
+		//}
+		return false;
+	})
+}
+
+
+function fieldmap_fields(wordpress_object, salesforce_object, row_count) {
+	var data = {
+		'action' : 'get_wp_sf_object_fields',
+		'wordpress_object' : wordpress_object,
+		'salesforce_object' : salesforce_object
+	}
+	$.post(ajaxurl, data, function(response) {
+
+		var wordpress = '';
+		wordpress += '<select name="wordpress_field" id="wordpress_field">'
+		$.each(response.data.wordpress, function(index, value) {
+			wordpress += '<option value="' + value.meta_key + '">' + value.meta_key + '</option>';
 		});
+		wordpress += '</select>';
+
+		var salesforce = '';
+		salesforce += '<select name="salesforce_field" id="salesforce_field">'
+		$.each(response.data.salesforce, function(index, value) {
+			salesforce += '<option value="' + value.label + '">' + value.label + '</option>';
+		});
+		salesforce += '</select>';
+
+		var markup = '<tr><td class="column-wordpress_field">' + wordpress + '</td><td class="column-salesforce_field">' + salesforce + '</td><td class="column-is_key"><input type="checkbox" name="is_key[' + row_count + ']" id="is_key-' + row_count + '" /></td><td class="column-direction"><div class="radios"><label><input type="radio" value="sf_wp" name="direction[0]" id="direction-' + row_count + '-sf-wp">  Salesforce to WordPress</label><label><input type="radio" value="wp_sf" name="direction[0]" id="direction-' + row_count + '-wp-sf">  WordPress to Salesforce</label><label><input type="radio" value="sf_wp" name="direction[0]" id="direction-' + row_count + '-sync">  Sync</label></div></td><td class="column-is_delete"><input type="checkbox" name="is_delete[' + row_count + ']" id="is_delete-' + row_count + '" /></td></tr>';
+		$('table.fields tbody').append(markup);
+
+	});
+}
+
+$(document).ready(function() {
+
+	$('#wordpress_object, #salesforce_object').on('change', function() {
+		$('table.fields tbody tr').remove();
+	})
+
+	$(document).ajaxStart(function(){
+		$('.spinner').addClass('is-active');
+	}).ajaxStop(function() {
+		$('.spinner').removeClass('is-active');
+	});
 	salesforce_date_fields();
+	add_field_mapping_row();
 });
