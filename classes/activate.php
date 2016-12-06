80a253bbfc0926598c47eaa05e604ec4d8c48c4d
Jonathan Stegall <jstegall@minnpost.com>
2016-12-06 14:32:43 -0600

    add comments, and remove the &$, from activate and deactivate

--- classes/activate.php
+++ classes/activate.php
@@ -12,7 +12,10 @@ class Wordpress_Salesforce_Activate {
 
     /**
     * What to do when the plugin is activated
+    *
+    * @param object $wpdb
     * @param string $version
+    * @param string $text_domain
     *
     */
     public function __construct( $wpdb, $version, $text_domain ) {
@@ -23,45 +26,50 @@ class Wordpress_Salesforce_Activate {
         add_action( 'plugins_loaded', array( &$this, 'wordpress_salesforce_update_db_check' ) );
     }
 
+    /**
+    * Create database tables for Salesforce
+    * This creates tables for fieldmaps (between types of objects) and object maps (between indidual instances of objects)
+    *
+    */ 
     public function wordpress_salesforce_tables() {
 
         $charset_collate = $this->wpdb->get_charset_collate();
-        
+
         $field_map_table = $this->wpdb->prefix . 'salesforce_field_map';
         $field_map_sql = "CREATE TABLE $field_map_table (
-          id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-          label varchar(64) NOT NULL DEFAULT '',
-          name varchar(64) NOT NULL DEFAULT '',
-          wordpress_object varchar(128) NOT NULL DEFAULT '',
-          salesforce_object varchar(255) NOT NULL DEFAULT '',
-          salesforce_record_types_allowed longblob,
-          salesforce_record_type_default varchar(255) NOT NULL DEFAULT '',
-          fields text NOT NULL,
-          pull_trigger_field varchar(128) NOT NULL DEFAULT 'LastModifiedDate',
-          sync_triggers text NOT NULL,
-          push_async tinyint(1) NOT NULL DEFAULT '0',
-          ignore_drafts tinyint(1) NOT NULL DEFAULT '0',
-          weight tinyint(1) NOT NULL DEFAULT '0',
-          PRIMARY KEY  (id)
+            id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+            label varchar(64) NOT NULL DEFAULT '',
+            name varchar(64) NOT NULL DEFAULT '',
+            wordpress_object varchar(128) NOT NULL DEFAULT '',
+            salesforce_object varchar(255) NOT NULL DEFAULT '',
+            salesforce_record_types_allowed longblob,
+            salesforce_record_type_default varchar(255) NOT NULL DEFAULT '',
+            fields text NOT NULL,
+            pull_trigger_field varchar(128) NOT NULL DEFAULT 'LastModifiedDate',
+            sync_triggers text NOT NULL,
+            push_async tinyint(1) NOT NULL DEFAULT '0',
+            ignore_drafts tinyint(1) NOT NULL DEFAULT '0',
+            weight tinyint(1) NOT NULL DEFAULT '0',
+            PRIMARY KEY  (id)
         ) $charset_collate";
 
         $object_map_table = $this->wpdb->prefix . 'salesforce_object_map';
         $object_map_sql = "CREATE TABLE $object_map_table (
-          id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-          wordpress_id bigint(20) NOT NULL,
-          salesforce_id varchar(32) NOT NULL DEFAULT '',
-          wordpress_object varchar(128) NOT NULL DEFAULT '',
-          created datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-          object_updated datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-          last_sync datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-          last_sync_action varchar(128) DEFAULT NULL,
-          last_sync_status tinyint(1) NOT NULL DEFAULT '0',
-          last_sync_message varchar(255) DEFAULT NULL,
-          PRIMARY KEY  (id),
-          UNIQUE KEY salesforce (salesforce_id),
-          UNIQUE KEY salesforce_wordpress (wordpress_object,wordpress_id),
-          KEY wordpress_object (wordpress_object,wordpress_id),
-          KEY salesforce_object (salesforce_id)
+            id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+            wordpress_id bigint(20) NOT NULL,
+            salesforce_id varchar(32) NOT NULL DEFAULT '',
+            wordpress_object varchar(128) NOT NULL DEFAULT '',
+            created datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+            object_updated datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+            last_sync datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+            last_sync_action varchar(128) DEFAULT NULL,
+            last_sync_status tinyint(1) NOT NULL DEFAULT '0',
+            last_sync_message varchar(255) DEFAULT NULL,
+            PRIMARY KEY  (id),
+            UNIQUE KEY salesforce (salesforce_id),
+            UNIQUE KEY salesforce_wordpress (wordpress_object,wordpress_id),
+            KEY wordpress_object (wordpress_object,wordpress_id),
+            KEY salesforce_object (salesforce_id)
         ) $charset_collate";
 
         require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
@@ -72,6 +80,11 @@ class Wordpress_Salesforce_Activate {
 
     }
 
+    /**
+    * Check for database version on plugin upgrade
+    * When the plugin is upgraded, if the database version does not match the current version, perform these methods
+    *
+    */ 
     public function wordpress_salesforce_update_db_check() {
         if ( get_site_option( 'salesforce_rest_api_db_version' ) != $this->version ) {
             $this->wordpress_salesforce_tables();
--- classes/deactivate.php
+++ classes/deactivate.php
@@ -11,6 +11,7 @@ class Wordpress_Salesforce_Deactivate {
 
     /**
     * What to do when the plugin is deactivated
+    * @param object $wpdb
     * @param string $version
     *
     */
@@ -18,11 +19,16 @@ class Wordpress_Salesforce_Deactivate {
         $this->wpdb = &$wpdb;
         $this->version = $version;
         $this->schedulable_classes = $schedulable_classes;
-        register_deactivation_hook( dirname( __DIR__ ) . '/' . $text_domain . '.php', array( &$this, 'wordpress_salesforce_drop_tables' ) );
-        register_deactivation_hook( dirname( __DIR__ ) . '/' . $text_domain . '.php', array( &$this, 'clear_schedule' ) );
-        register_deactivation_hook( dirname( __DIR__ ) . '/' . $text_domain . '.php', array( &$this, 'delete_log_post_type' ) );
+        register_deactivation_hook( dirname( __DIR__ ) . '/' . $text_domain . '.php', array( $this, 'wordpress_salesforce_drop_tables' ) );
+        register_deactivation_hook( dirname( __DIR__ ) . '/' . $text_domain . '.php', array( $this, 'clear_schedule' ) );
+        register_deactivation_hook( dirname( __DIR__ ) . '/' . $text_domain . '.php', array( $this, 'delete_log_post_type' ) );
     }
 
+    /**
+    * Drop database tables for Salesforce
+    * This removes the tables for fieldmaps (between types of objects) and object maps (between indidual instances of objects)
+    *
+    */ 
     public function wordpress_salesforce_drop_tables() {
         $field_map_table = $this->wpdb->prefix . 'salesforce_field_map';
         $object_map_table = $this->wpdb->prefix . 'salesforce_object_map';
@@ -31,12 +37,22 @@ class Wordpress_Salesforce_Deactivate {
         delete_option( 'salesforce_rest_api_db_version' );
     }
 
+    /**
+    * Clear the scheduled tasks
+    * This removes all the scheduled tasks that are included in the plugin's $schedulable_classes array
+    *
+    */ 
     public function clear_schedule() {
         foreach ( $this->schedulable_classes as $key => $value ) {
             wp_clear_scheduled_hook( $key );
         }
     }
 
+    /**
+    * Delete the log post type
+    * This removes the log post type
+    *
+    */ 
     public function delete_log_post_type() {
         unregister_post_type( 'wp_log' );
     }
