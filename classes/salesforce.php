MinnPost/object-sync-for-salesforce

commit c9e5337d44f4870c23cc3d0bb5d5a3588080e540

some uppercase on TRUE and FALSE wooo

classes/salesforce.php
@@ -226,7 +226,7 @@ class Salesforce {
 	protected function api_http_request( $path, $params, $method, $options = array(), $type = 'rest' ) {
 		$options = array_merge( $this->options, $options ); // this will override a value in $this->options with the one in $options if there is a matching key
 		$url = $this->get_api_endpoint( $type ) . $path;
-		if ( isset( $options['full_url'] ) && $options['full_url'] === true ) {
+		if ( isset( $options['full_url'] ) && $options['full_url'] === TRUE ) {
 			$url = $path;
 		}
 		$headers = array(
@@ -237,39 +237,39 @@ class Salesforce {
 		if ( $method === 'POST' || $method === 'PATCH' ) {
 			$headers['Content-Type'] = 'Content-Type: application/json';
 		}
-		if ( isset( $options['authenticated'] ) && $options['authenticated'] === true ) {
-			$headers = false;
+		if ( isset( $options['authenticated'] ) && $options['authenticated'] === TRUE ) {
+			$headers = FALSE;
 		}
 		// if this request should be cached, see if it already exists
 		// if it is already cached, load it. if not, load it and then cache it if it should be cached
 		// add parameters to the array so we can tell if it was cached or not
-		if ( $options['cache'] === true && $options['type'] !== 'write' ) { 
+		if ( $options['cache'] === TRUE && $options['type'] !== 'write' ) { 
 			$cached = $this->wordpress->cache_get( $url, $params );
 			if ( is_array( $cached ) ) {
 				$result = $cached;
-				$result['from_cache'] = true;
-				$result['cached'] = true;
+				$result['from_cache'] = TRUE;
+				$result['cached'] = TRUE;
     		} else {
     			$data = json_encode( $params );
 				$result = $this->http_request( $url, $data, $headers, $method, $options );
 				if ( in_array( $result['code'], $this->success_codes ) ) {
 					$result['cached'] = $this->wordpress->cache_set( $url, $params, $result, $options['cache_expiration'] );
 				} else {
-					$result['cached'] = false;
+					$result['cached'] = FALSE;
 				}
-				$result['from_cache'] = false;
+				$result['from_cache'] = FALSE;
     		}
 		} else {
 			$data = json_encode( $params );
 			$result = $this->http_request( $url, $data, $headers, $method, $options );
-			$result['from_cache'] = false;
-			$result['cached'] = false;
+			$result['from_cache'] = FALSE;
+			$result['cached'] = FALSE;
 		}
 
-		if ( isset( $options['is_redo'] ) && $options['is_redo'] === true ) {
-			$result['is_redo'] = true;
+		if ( isset( $options['is_redo'] ) && $options['is_redo'] === TRUE ) {
+			$result['is_redo'] = TRUE;
 		} else {
-			$result['is_redo'] = false;
+			$result['is_redo'] = TRUE;
 		}
 
 		return $result;

classes/admin.php
@@ -128,7 +128,7 @@ class Wordpress_Salesforce_Admin {
                         if ( isset( $_GET['code'] ) )  {
                             $is_authorized = $this->salesforce['sfapi']->request_token( esc_attr( $_GET['code'] ) );
                             echo "<script>window.location = '$callback_url';</script>";
-                        } elseif ( $this->salesforce['is_authorized'] === true ) {
+                        } elseif ( $this->salesforce['is_authorized'] === TRUE ) {
                             require_once( plugin_dir_path( __FILE__ ) . '/../templates/admin/authorized.php' );
                             $this->demo( $this->salesforce['sfapi'] );
                         } elseif ( isset( $consumer_key ) && isset( $consumer_secret ) ) {
@@ -665,10 +665,10 @@ class Wordpress_Salesforce_Admin {
     * @return array $object_settings
     */
     public function get_salesforce_object_description( $data = array() ) {
-        $ajax = false;
+        $ajax = FALSE;
         if ( empty( $data ) ) {
             $data = $_POST;
-            $ajax = true;
+            $ajax = TRUE;
         }
 
         $object_description = array();
@@ -703,7 +703,7 @@ class Wordpress_Salesforce_Admin {
             }
         }
 
-        if ( $ajax === true ) {
+        if ( $ajax === TRUE ) {
             wp_send_json_success( $object_description );
         } else {
             return $object_description;
@@ -752,15 +752,15 @@ class Wordpress_Salesforce_Admin {
     * @return array $object_fields
     */
     public function get_wordpress_object_fields( $wordpress_object = '' ) {
-        $ajax = false;
+        $ajax = FALSE;
         if ( empty( $wordpress_object ) ) {
             $wordpress_object = $_POST['wordpress_object'];
-            $ajax = true;
+            $ajax = TRUE;
         }
         
         $object_fields = $this->wordpress->get_wordpress_object_fields( $wordpress_object );
         
-        if ( $ajax === true ) {
+        if ( $ajax === TRUE ) {
             wp_send_json_success( $object_fields );
         } else {
             return $object_fields;
@@ -863,13 +863,13 @@ class Wordpress_Salesforce_Admin {
     *
     */
     public function prepare_fieldmap_data() {
-        $error = false;
+        $error = FALSE;
         $cachekey = md5( json_encode( $_POST ) );
         
         if ( !isset( $_POST['label'] ) || !isset( $_POST['salesforce_object'] ) || !isset( $_POST['wordpress_object'] ) ) {
-            $error = true;
+            $error = TRUE;
         }
-        if ( $error === true ) {
+        if ( $error === TRUE ) {
             set_transient( $cachekey, $_POST, 0 );
             if ( $cachekey !== '' ) {
                 $url = esc_url_raw( $_POST['redirect_url_error'] ) . '&transient=' . $cachekey;
@@ -886,7 +886,7 @@ class Wordpress_Salesforce_Admin {
                 $id = esc_attr( $_POST['id'] );
                 $result = $this->mappings->update_fieldmap( $_POST, $wordpress_fields, $salesforce_fields, $id );
             }
-            if ( $result === false ) { // if the database didn't save, it's still ane rror
+            if ( $result === FALSE ) { // if the database didn't save, it's still ane rror
                 set_transient( $cachekey, $_POST, 0 );
                 if ( $cachekey !== '' ) {
                     $url = esc_url_raw( $_POST['redirect_url_error'] ) . '&transient=' . $cachekey;
@@ -913,7 +913,7 @@ class Wordpress_Salesforce_Admin {
     public function delete_fieldmap() {
         if ( $_POST['id'] ) {
             $result = $this->mappings->delete_fieldmap( $_POST['id'] );
-            if ( $result === true ) {
+            if ( $result === TRUE ) {
                 $url = esc_url_raw( $_POST['redirect_url_success'] );
             } else {
                 $url = esc_url_raw( $_POST['redirect_url_error'] ) . '&id=' . $_POST['id'];
@@ -1090,18 +1090,18 @@ class Wordpress_Salesforce_Admin {
         $versions = $sfapi->get_api_versions();
 
         // format this array into html so users can see the versions
-        $versions_is_cached = $versions['cached'] === true ? '' : 'not ';
-        $versions_from_cache = $versions['from_cache'] === true ? 'were' : 'were not';
-        $versions_is_redo = $versions['is_redo'] === true ? '' : 'not ';
-        $versions_andorbut = $versions['from_cache'] === true ? 'and' : 'but';
+        $versions_is_cached = $versions['cached'] === TRUE ? '' : 'not ';
+        $versions_from_cache = $versions['from_cache'] === TRUE ? 'were' : 'were not';
+        $versions_is_redo = $versions['is_redo'] === TRUE ? '' : 'not ';
+        $versions_andorbut = $versions['from_cache'] === TRUE ? 'and' : 'but';
 
         $contacts = $sfapi->query('SELECT Name, Id from Contact LIMIT 100');
 
         // format this array into html so users can see the contacts
-        $contacts_is_cached = $contacts['cached'] === true ? '' : 'not ';
-        $contacts_from_cache = $contacts['from_cache'] === true ? 'were' : 'were not';
-        $contacts_andorbut = $contacts['from_cache'] === true ? 'and' : 'but';
-        $contacts_is_redo = $contacts['is_redo'] === true ? '' : 'not ';
+        $contacts_is_cached = $contacts['cached'] === TRUE ? '' : 'not ';
+        $contacts_from_cache = $contacts['from_cache'] === TRUE ? 'were' : 'were not';
+        $contacts_andorbut = $contacts['from_cache'] === TRUE ? 'and' : 'but';
+        $contacts_is_redo = $contacts['is_redo'] === TRUE ? '' : 'not ';
 
         require_once( plugin_dir_path( __FILE__ ) . '/../templates/admin/demo.php' );
 

commit fb7b0872e9dca7db3f3b0ef95f5403f5021b747f

fixed a stupid bug i caused in c9e5337d44f4870c23cc3d0bb5d5a3588080e540

classes/salesforce.php
@@ -168,36 +168,33 @@ class Salesforce {
 				// Refresh token.
 				$this->refresh_token();
 				// Rebuild our request and repeat request.
-				$options['is_redo'] = true;
+				$options['is_redo'] = TRUE;
 				$this->response = $this->api_http_request( $path, $params, $method, $options, $type );
 				// Throw an error if we still have bad response.
 				if ( !in_array( $this->response['code'], $this->success_codes ) ) {
 					throw new SalesforceException( $this->response['data'][0]['message'], $this->response['code'] );
 				}
-
-			break;
-
-		case in_array( $this->response['code'], $this->success_codes ):
-			// All clear.
-			break;
-
-		default:
-			// We have problem and no specific Salesforce error provided.
-			if ( empty( $this->response['data'] ) ) {
-				throw new SalesforceException( $this->response['error'], $this->response['code'] );
-			}
+				break;
+			case in_array( $this->response['code'], $this->success_codes ):
+				// All clear.
+				break;
+			default:
+				// We have problem and no specific Salesforce error provided.
+				if ( empty( $this->response['data'] ) ) {
+					throw new SalesforceException( $this->response['error'], $this->response['code'] );
+				}
 		}
 
 		if ( !empty( $this->response['data'][0] ) && count( $this->response['data'] ) == 1 ) {
-		  $this->response['data'] = $this->response['data'][0];
+			$this->response['data'] = $this->response['data'][0];
 		}
 
 		if ( isset( $this->response['data']['error'] ) ) {
-		  throw new SalesforceException( $this->response['data']['error_description'], $this->response['data']['error'] );
+			throw new SalesforceException( $this->response['data']['error_description'], $this->response['data']['error'] );
 		}
 
 		if ( !empty($this->response['data']['errorCode'] ) ) {
-		  throw new SalesforceException( $this->response['data']['message'], $this->response['code'] );
+			throw new SalesforceException( $this->response['data']['message'], $this->response['code'] );
 		}
 		
 		return $this->response;
@@ -269,7 +266,7 @@ class Salesforce {
 		if ( isset( $options['is_redo'] ) && $options['is_redo'] === TRUE ) {
 			$result['is_redo'] = TRUE;
 		} else {
-			$result['is_redo'] = TRUE;
+			$result['is_redo'] = FALSE;
 		}
 
 		return $result;
@@ -557,7 +554,6 @@ class Salesforce {
 			),
 			$this->login_url . $this->authorize_path
 		);
-		error_log('url is ' . $url);
 		return $url;
 	}
 