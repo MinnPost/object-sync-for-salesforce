82368c4c5de032e835e094fe6c040988313848b3
Jonathan Stegall <jstegall@minnpost.com>
2017-05-24 12:01:59 -0500

    small template changes for #87. escaping and getting rid of linter errors. there are more changes in the other template files though.

--- templates/admin/user-profile-salesforce-change.php
+++ templates/admin/user-profile-salesforce-change.php
@@ -2,11 +2,11 @@
 <h2>Salesforce</h2>
 <p>You can change the Salesforce object that this WordPress user maps to by changing the ID and updating this user.</p>
 <table class="form-table">
-    <tr>
-        <th><label for="salesforce_id">Salesforce ID</label></th>
-        <td>
-			<input type="text" name="salesforce_id" id="salesforce_id" value="<?php if ( isset( $mapping['id'] ) ) { echo $mapping['salesforce_id']; } ?>" class="regular-text" /><br />
-			<span class="description">Enter a Salesforce object ID.</span>
+	<tr>
+		<th><label for="salesforce_id"><?php echo esc_html__( 'Salesforce ID', 'object-sync-for-salesforce' ); ?></label></th>
+		<td>
+			<input type="text" name="salesforce_id" id="salesforce_id" value="<?php if ( isset( $mapping['id'] ) ) { echo esc_html( $mapping['salesforce_id'] ); } ?>" class="regular-text" /><br />
+			<span class="description"><?php echo esc_html__( 'Enter a Salesforce object ID.', 'object-sync-for-salesforce' ); ?></span>
 		</td>
 	</tr>
 </table>
