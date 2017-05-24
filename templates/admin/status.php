4b008caefb059cbd0104782261a5317f2170d58c
Jonathan Stegall <jstegall@minnpost.com>
2017-05-24 12:15:28 -0500

    update salesforce status page for #87

--- templates/admin/status.php
+++ templates/admin/status.php
@@ -1,14 +1,21 @@
-<h3>Salesforce Status</h3>
-<p>Currently, we are using version <?php echo $this->login_credentials['rest_api_version']; ?> of the Salesforce REST API. Available versions are displayed below.</p>
+<h3><?php echo esc_html__( 'Salesforce Status', 'object-sync-for-salesforce' ); ?></h3>
+<p>
+<?php
+	// translators: placeholder is for the version number of the Salesforce REST API
+	echo sprintf( esc_html__( 'Currently, we are using version %1$s of the Salesforce REST API. Available versions are displayed below.', 'object-sync-for-salesforce' ),
+		esc_html( $this->login_credentials['rest_api_version'] )
+	);
+?>
+</p>
 <table class="widefat striped">
 	<thead>
 		<summary>
-			<h4>Available Salesforce API versions. This list is <?php echo $versions_is_cached; ?> cached, <?php echo $versions_andorbut; ?> items <?php echo $versions_from_cache; ?> loaded from the cache. This is not an authenticated request, so it does not touch the Salesforce token.</h4>
+			<h4>Available Salesforce API versions. This list is <?php echo esc_html( $versions_is_cached ); ?> cached, <?php echo esc_html( $versions_andorbut ); ?> items <?php echo esc_html( $versions_from_cache ); ?> loaded from the cache. This is not an authenticated request, so it does not touch the Salesforce token.</h4>
 		</summary>
 		<tr>
-			<th>Label</th>
-			<th>URL</th>
-			<th>Version</th>
+			<th><?php echo esc_html__( 'Label', 'object-sync-for-salesforce' ); ?></th>
+			<th><?php echo esc_html__( 'URL', 'object-sync-for-salesforce' ); ?></th>
+			<th><?php echo esc_html__( 'Version', 'object-sync-for-salesforce' ); ?></th>
 		</tr>
 	</thead>
 	<tbody>
@@ -19,10 +26,10 @@
 				$class = ' class="current"';
 			}
 			?>
-			<tr<?php echo $class; ?>>
-				<td><?php echo $version['label']; ?></td>
-				<td><?php echo $version['url']; ?></td>
-				<td><?php echo $version['version']; ?></td>
+			<tr<?php echo esc_attr( $class ); ?>>
+				<td><?php echo esc_html( $version['label'] ); ?></td>
+				<td><?php echo esc_html( $version['url'] ); ?></td>
+				<td><?php echo esc_html( $version['version'] ); ?></td>
 			</tr>
 		<?php } ?>
 	</tbody>
@@ -31,18 +38,18 @@
 <table class="widefat striped">
 	<thead>
 		<summary>
-			<h4>Salesforce successfully returned <?php echo $contacts['data']['totalSize']; ?> <?php echo $contacts['data']['records'][0]['attributes']['type']; ?> records. They are <?php echo $contacts_is_cached; ?>cached, <?php echo $contacts_andorbut; ?> they <?php echo $contacts_from_cache; ?> loaded from the cache. This request did <?php echo $contacts_is_redo; ?>require refreshing the Salesforce token.</h4>
+			<h4>Salesforce successfully returned <?php echo absint( $contacts['data']['totalSize'] ); ?> <?php echo esc_html( $contacts['data']['records'][0]['attributes']['type'] ); ?> records. They are <?php echo esc_html( $contacts_is_cached ); ?>cached, <?php echo esc_html( $contacts_andorbut ); ?> they <?php echo esc_html( $contacts_from_cache ); ?> loaded from the cache. This request did <?php echo esc_html( $contacts_is_redo ); ?>require refreshing the Salesforce token.</h4>
 		</summary>
 		<tr>
-			<th>Contact ID</th>
-			<th>Name</th>
+			<th><?php echo esc_html__( 'Contact ID' ); ?></th>
+			<th><?php echo esc_html__( 'Name' ); ?></th>
 		</tr>
 	</thead>
 	<tbody>
 		<?php foreach ( $contacts['data']['records'] as $contact ) { ?>
 			<tr>
-				<td><?php echo $contact['Id']; ?></td>
-				<td><?php echo $contact['Name']; ?></td>
+				<td><?php echo esc_html( $contact['Id'] ); ?></td>
+				<td><?php echo esc_html( $contact['Name'] ); ?></td>
 			</tr>
 		<?php } ?>
 	</tbody>
