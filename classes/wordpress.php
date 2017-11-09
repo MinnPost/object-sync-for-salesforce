adc1b5513295421e1f7c792bc76f7c20eb5eff2f
Jonathan Stegall <jstegall@minnpost.com>
2017-11-09 15:02:45 -0600

    class to manage all cache values for this plugin by putting their names into an array that we can go through if we need to clear it.

--- classes/wordpress.php
+++ classes/wordpress.php
@@ -2395,3 +2395,90 @@ class Object_Sync_Sf_WordPress {
  */
 class WordpressException extends Exception {
 }
+
+/**
+ * Class to store all theme/plugin transients as an array in one WordPress transient
+ **/
+class Object_Sync_Sf_WordPress_Transient {
+
+	protected $name;
+
+	/**
+	 * Constructor which sets cache options and the name of the field that lists this plugin's cache keys.
+	 *
+	 * @param string $name The name of the field that lists all cache keys.
+	 */
+	public function __construct( $name ) {
+		$this->name = $name;
+		$this->cache_prefix = esc_sql( 'sfwp_' );
+	}
+
+	/**
+	 * Get the transient that lists all the other transients for this plugin.
+	 *
+	 * @return mixed value of transient. False of empty, otherwise array.
+	 */
+	public function all_keys() {
+		return get_transient( $this->name );
+	}
+
+	/**
+	 * Set individual transient, and add its key to the list of this plugin's transients.
+	 *
+	 * @param string $cachekey the key for this cache item
+	 * @param mixed $value the value of the cache item
+	 * @param int $cache_expiration. How long the plugin key cache, and this individual item cache, should last before expiring.
+	 * @return mixed value of transient. False of empty, otherwise array.
+	 */
+	public function set( $cachekey, $value, $cache_expiration = 0 ) {
+
+		$prefix = $this->cache_prefix;
+		$cachekey = $prefix . $cachekey;
+
+		$keys = $this->all_keys();
+		$keys[] = $cachekey;
+		set_transient( $this->name, $keys, $cache_expiration );
+
+		return set_transient( $cachekey, $value, $cache_expiration );
+	}
+
+	/**
+	 * Get the individual cache value
+	 *
+	 * @param string $cachekey the key for this cache item
+	 * @return mixed value of transient. False of empty, otherwise array.
+	 */
+	public function get( $cachekey ) {
+		$prefix = $this->cache_prefix;
+		$cachekey = $prefix . $cachekey;
+		return get_transient( $cachekey );
+	}
+
+	/**
+	 * Delete the individual cache value
+	 *
+	 * @param string $cachekey the key for this cache item
+	 * @return bool True if successful, false otherwise.
+	 */
+	public function delete( $cachekey ) {
+		$prefix = $this->cache_prefix;
+		$cachekey = $prefix . $cachekey;
+		return delete_transient( $cachekey );
+	}
+
+	/**
+	 * Delete the entire cache for this plugin
+	 *
+	 * @return bool True if successful, false otherwise.
+	 */
+	public function flush() {
+		$keys = $this->all_keys();
+		$result = true;
+		foreach ( $keys as $key ) {
+			$result = delete_transient( $key );
+		}
+		$result = delete_transient( $this->name );
+		return $result;
+	}
+
+}
