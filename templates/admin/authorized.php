2ed54468d518ef43cfdbd1d8ee52060a426c9f32
Jonathan Stegall <jstegall@minnpost.com>
2017-11-09 14:22:59 -0600

    the logout url was wrong

--- templates/admin/authorized.php
+++ templates/admin/authorized.php
@@ -1,2 +1,2 @@
 <div class="success"><h2><?php echo esc_html__( 'Salesforce is successfully authenticated.' ); ?></h2></div>
-<p><a class="button button-primary" href="<?php echo esc_url( $callback_url ); ?>&amp;tab=logout"><?php echo esc_html__( 'Disconnect from Salesforce', 'object-sync-for-salesforce' ); ?></a></p>
+<p><a class="button button-primary" href="<?php echo esc_url( get_admin_url( null, 'options-general.php?page=object-sync-salesforce-admin&tab=logout' ) ); ?>"><?php echo esc_html__( 'Disconnect from Salesforce', 'object-sync-for-salesforce' ); ?></a></p>
