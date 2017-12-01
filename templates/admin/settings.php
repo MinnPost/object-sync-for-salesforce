5c220cdfb4f22266165c9e4805db7b56113f11b2
Jonathan Stegall <jstegall@minnpost.com>
2017-12-01 09:24:43 -0600

    escape string

--- templates/admin/settings.php
+++ templates/admin/settings.php
@@ -1,6 +1,6 @@
 <form method="post" action="options.php">
 	<?php
 		settings_fields( $tab ) . do_settings_sections( $tab );
-		submit_button( 'Save settings' );
+		submit_button( esc_html__( 'Save settings', 'object-sync-for-salesforce' ) );
 	?>
 </form>
