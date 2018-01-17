2eafe9674e6a752c2d80ed7ad4e96c206a8871a9
Jonathan Stegall <jstegall@minnpost.com>
2018-01-17 12:41:31 -0600

    stupid typo

--- classes/deactivate.php
+++ classes/deactivate.php
@@ -113,7 +113,7 @@ class Object_Sync_Sf_Deactivate {
 	*
 	*/
 	public function delete_plugin_options() {
-		$plugin_options = $wpdb->get_results( "SELECT option_name FROM $wpdb->options WHERE option_name LIKE 'object_sync_for_salesforce_%'" );
+		$plugin_options = $this->wpdb->get_results( "SELECT option_name FROM $this->wpdb->options WHERE option_name LIKE 'object_sync_for_salesforce_%'" );
 		foreach ( $plugin_options as $option ) {
 			delete_option( $option->option_name );
 		}
