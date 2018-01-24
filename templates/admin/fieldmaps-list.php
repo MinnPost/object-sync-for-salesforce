9a3167f09e564dce8e2eba75463e35bcc65b0359
Jonathan Stegall <jstegall@minnpost.com>
2018-01-24 12:06:08 -0600

    oh haha that markup was messed up. not worth a release, but worth fixing for next release.

--- templates/admin/fieldmaps-list.php
+++ templates/admin/fieldmaps-list.php
@@ -14,12 +14,11 @@
 				<p><small>
 					<?php
 					// translators: the placeholders refer to: 1) the cache clear link, 2) the cache clear link text
-					echo sprintf( esc_html__( 'Has your WordPress or Salesforce data structure changed? ', 'object-sync-for-salesforce' ) . '<a href="%1$s" id="clear-sfwp-cache">%2$s</a> to make sure you can map the most recent data structures.',
+					echo sprintf( esc_html__( 'Has your WordPress or Salesforce data structure changed? ', 'object-sync-for-salesforce' ) . '<a href="%1$s" id="clear-sfwp-cache">%2$s</a>' . esc_html__( ' to make sure you can map the most recent data structures.', 'object-sync-for-salesforce' ),
 						esc_url( get_admin_url( null, 'options-general.php?page=object-sync-salesforce-admin&tab=clear_cache' ) ),
 						esc_html__( 'Clear the plugin cache' )
 					);
-				?>
-				</p>
+					?>
 				</small></p>
 			</td>
 		</tr>
