9eff5dff71f52bf8e2216380907ab58d16f774e1
Jonathan Stegall <jstegall@minnpost.com>
2018-03-09 17:15:03 -0600

    add a hook for #152

--- classes/salesforce_push.php
+++ classes/salesforce_push.php
@@ -690,6 +690,13 @@ class Object_Sync_Sf_Salesforce_Push {
 				// ex: run WordPress methods on an object if it exists, or do something in preparation for it if it doesn't
 				do_action( 'object_sync_for_salesforce_pre_push', $salesforce_id, $mapping, $object, $object_id, $params );
 
+				// hook to allow other plugins to change params on update actions only
+				// use hook to map fields between the WordPress and Salesforce objects
+				// returns $params.
+				if ( null !== $salesforce_id ) {
+					$params = apply_filters( 'object_sync_for_salesforce_push_update_params_modify', $params, $salesforce_id, $mapping, $object );
+				}
+
 				if ( isset( $prematch_field_wordpress ) || isset( $key_field_wordpress ) || null !== $salesforce_id ) {
 
 					// if either prematch criteria exists, make the values queryable
@@ -914,6 +921,11 @@ class Object_Sync_Sf_Salesforce_Push {
 				// hook to allow other plugins to do something right before Salesforce data is saved
 				// ex: run WordPress methods on an object if it exists, or do something in preparation for it if it doesn't
 				do_action( 'object_sync_for_salesforce_pre_push', $mapping_object['salesforce_id'], $mapping, $object, $object_id, $params );
+
+				// hook to allow other plugins to change params on update actions only
+				// use hook to map fields between the WordPress and Salesforce objects
+				// returns $params.
+				$params = apply_filters( 'object_sync_for_salesforce_push_update_params_modify', $params, $mapping_object['salesforce_id'], $mapping, $object );
 
 				$op = 'Update';
 				$result = $sfapi->object_update( $mapping['salesforce_object'], $mapping_object['salesforce_id'], $params );
