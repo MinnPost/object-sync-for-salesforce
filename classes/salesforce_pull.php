9541d4f45e277edeeebf3549feb4bb46b64dc251
Jonathan Stegall <jstegall@minnpost.com>
2019-01-14 12:14:32 -0600

    merge rest api changes into salesforce_pull. still need to see if it can be streamlined any further.

--- classes/salesforce_pull.php
+++ classes/salesforce_pull.php
@@ -96,6 +96,7 @@ class Object_Sync_Sf_Salesforce_Pull {
 
 	/**
 	* Create the action hooks based on what object maps exist from the admin settings
+	* route is http://example.com/wp-json/salesforce-rest-api/pull/ plus params we decide to accept
 	*
 	*/
 	public function add_actions() {
@@ -109,32 +110,45 @@ class Object_Sync_Sf_Salesforce_Pull {
 	}
 
 	/**
-	* Ajax callback for salesforce pull. Returns status of 200 for successful
+	* REST API callback for salesforce pull. Returns status of 200 for successful
 	* attempt or 403 for a failed pull attempt (SF not authorized, threshhold
 	* reached, etc.
-	* this is the ajax callback; not a cron run
+	*
+	* @param object $request
+	* This is a merged object of all the arguments from the API request
+	* @return array
+	* code: 201
+	* data:
+	*   success : true
+	*
 	*/
-	public function salesforce_pull_webhook() {
+	public function salesforce_pull_webhook( WP_REST_Request $request ) {
 
-		if ( true === $this->salesforce_pull() ) {
-			$code = '200';
+		// run a pull request and then run the schedule if anything is in there
+		$data = $this->salesforce_pull();
 
+		// salesforce_pull currently returns true if it runs successfully
+		if ( true === $data ) {
+			$code = '201';
+			// check to see if anything is in the queue and handle it if it is
 			// single task for action-scheduler to check for data
 			$this->queue->add(
 				$this->schedulable_classes[ $this->schedule_name ]['initializer'],
 				array(),
 				$this->schedule_name
 			);
-
 		} else {
 			$code = '403';
 		}
 
-		if ( ! empty( $_POST ) ) { // linter flags this, but we're not doing anything with the array just checking to see if it is empty
-			wp_send_json_success( $code );
-		} else {
-			return $code;
-		}
+		$result = array(
+			'code' => $code,
+			'data' => array(
+				'success' => $data,
+			),
+		);
+
+		return $result;
 
 	}
 
@@ -787,20 +801,132 @@ class Object_Sync_Sf_Salesforce_Pull {
 	*
 	* @param string $object_type
 	* @param string $salesforce_id
-	* @param string $wordpress_object
+	*
+	* @return array $result
 	*
 	*/
-	public function manual_pull( $object_type, $salesforce_id, $wordpress_object ) {
-		$sfapi   = $this->salesforce['sfapi'];
-		$object  = $sfapi->api_call( 'sobjects/' . $object_type . '/' . $salesforce_id );
-		$mapping = $this->mappings->get_fieldmaps(
-			null,
+	public function manual_pull( $object_type, $salesforce_id ) {
+		$sfapi  = $this->salesforce['sfapi'];
+		$object = $sfapi->object_read(
+			$object_type,
+			$salesforce_id,
 			array(
-				'salesforce_object' => $object_type,
-				'wordpress_object'  => $wordpress_object,
+				'cache' => false,
 			)
+		)['data'];
+
+		// if the object call does not return an error, continue
+		if ( ! isset( $object['errorCode'] ) ) {
+			$code                = '201';
+			$salesforce_mappings = $this->mappings->get_fieldmaps(
+				null,
+				array(
+					'salesforce_object' => $object_type,
+				)
+			);
+
+			// from drupal: if there is more than one mapping, don't throw exceptions
+			$hold_exceptions = count( $salesforce_mappings ) > 1;
+			$exception       = false;
+
+			$frequencies = $this->queue->get_frequencies();
+			$seconds     = reset( $frequencies )['frequency'] + 60;
+
+			$transients_to_delete = array();
+
+			$results = array();
+
+			foreach ( $salesforce_mappings as $mapping ) {
+
+				$map_sync_triggers = $mapping['sync_triggers']; // this sets which Salesforce triggers are allowed for the mapping
+
+				// If no lastupdate, get all records, else get records since last pull.
+				// this should be what keeps it from getting all the records, whether or not they've ever been updated
+				// we also use the option for when the plugin was installed, and don't go back further than that by default
+
+				$sf_activate_time = get_option( $this->option_prefix . 'activate_time', '' );
+				$sf_last_sync     = get_option( $this->option_prefix . 'pull_last_sync_' . $object_type, null );
+				if ( $sf_last_sync ) {
+					$last_sync = gmdate( 'Y-m-d\TH:i:s\Z', $sf_last_sync );
+				} else {
+					$activated = gmdate( 'Y-m-d\TH:i:s\Z', $sf_activate_time );
+				}
+
+				// if this record is new as of the last sync, use the create trigger
+				if ( isset( $object['CreatedDate'] ) && $object['CreatedDate'] > $last_sync ) {
+					$sf_sync_trigger = $this->mappings->sync_sf_create;
+				} else {
+					$sf_sync_trigger = $this->mappings->sync_sf_update;
+				}
+				$pull_allowed = $this->is_pull_allowed( $object_type, $object, $sf_sync_trigger, $mapping, $map_sync_triggers );
+
+				if ( true === $pull_allowed ) {
+					$result = $this->salesforce_pull_process_records( $object_type, $object, $mapping, $sf_sync_trigger );
+				} else {
+					$status = 'error';
+					// create log entry for not allowed manual pull
+					if ( isset( $this->logging ) ) {
+						$logging = $this->logging;
+					} elseif ( class_exists( 'Object_Sync_Sf_Logging' ) ) {
+						$logging = new Object_Sync_Sf_Logging( $this->wpdb, $this->version );
+					}
+
+					// translators: placeholders are: 1) the name of the WordPress object type, 2) the name of the Salesforce object, 3) the Salesforce Id value
+					$title = sprintf( esc_html__( 'Error: Manually pull WordPress %1$s not allowed (%2$s %3$s)', 'object-sync-for-salesforce' ),
+						esc_attr( $mapping['wordpress_object'] ),
+						esc_attr( $mapping['salesforce_object'] ),
+						esc_attr( $salesforce_id )
+					);
+
+					$result = array(
+						'title'   => $title,
+						'message' => '',
+						'trigger' => '',
+						'parent'  => 0,
+						'status'  => $status,
+					);
+
+					$logging->setup( $result );
+				}
+				$results[] = $result;
+			}
+		} else {
+			$code = '403';
+			// create log entry for failed pull
+			$status = 'error';
+			// translators: placeholders are: 1) the server error code
+			$title = sprintf( esc_html__( 'Error: %1$s Salesforce Pull', 'object-sync-for-salesforce' ),
+				esc_attr( $object['errorCode'] )
+			);
+
+			if ( isset( $this->logging ) ) {
+				$logging = $this->logging;
+			} elseif ( class_exists( 'Object_Sync_Sf_Logging' ) ) {
+				$logging = new Object_Sync_Sf_Logging( $this->wpdb, $this->version );
+			}
+
+			$result = array(
+				'title'   => $title,
+				'message' => $object['message'],
+				'parent'  => '',
+				'status'  => $status,
+				'trigger' => '',
+			);
+
+			$logging->setup( $result );
+
+			return $result;
+		}
+
+		$result = array(
+			'code' => $code,
+			'data' => array(
+				'success' => $results,
+			),
 		);
-		$this->salesforce_pull_process_records( $object_type, $object['data'], $mapping[0], $this->mappings->sync_sf_update );
+
+		return $result;
+
 	}
 
 	/**
