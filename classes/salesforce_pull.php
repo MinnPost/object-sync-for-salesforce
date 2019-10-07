77b644805a188be62213bdcc8eddecb343e76281
Jonathan Stegall <jstegall@minnpost.com>
2019-10-07 16:07:19 -0500

    we don't need to check the version. it always has a default of '' if it's not there

--- classes/salesforce_pull.php
+++ classes/salesforce_pull.php
@@ -1513,28 +1513,17 @@ class Object_Sync_Sf_Salesforce_Pull {
 				// we can then check to see if it has a mapping object
 				// we should only do this if the above hook didn't already set the $wordpress_id
 				if ( null === $wordpress_id ) {
-					if ( version_compare( $this->version, '1.9.0', '>=' ) ) {
-						$wordpress_id = $this->wordpress->object_upsert(
-							$salesforce_mapping['wordpress_object'],
-							$upsert_key,
-							$upsert_value,
-							$upsert_methods,
-							$params,
-							$salesforce_mapping['pull_to_drafts'],
-							true,
-							$salesforce_mapping['wordpress_object_default_status']
-						);
-					} else {
-						$wordpress_id = $this->wordpress->object_upsert(
-							$salesforce_mapping['wordpress_object'],
-							$upsert_key,
-							$upsert_value,
-							$upsert_methods,
-							$params,
-							$salesforce_mapping['pull_to_drafts'],
-							true
-						);
-					}
+					// we don't need to check the plugin version here because the default status is empty in the mapping class, if there's no value in the database
+					$wordpress_id = $this->wordpress->object_upsert(
+						$salesforce_mapping['wordpress_object'],
+						$upsert_key,
+						$upsert_value,
+						$upsert_methods,
+						$params,
+						$salesforce_mapping['pull_to_drafts'],
+						true,
+						$salesforce_mapping['wordpress_object_default_status']
+					);
 				}
 
 				// find out if there is a mapping object for this WordPress object already
@@ -1675,28 +1664,17 @@ class Object_Sync_Sf_Salesforce_Pull {
 
 				// now we can upsert the object in wp if we've gotten to this point
 				// this command will either create or update the object
-				if ( version_compare( $this->version, '1.9.0', '>=' ) ) {
-					$result = $this->wordpress->object_upsert(
-						$salesforce_mapping['wordpress_object'],
-						$upsert_key,
-						$upsert_value,
-						$upsert_methods,
-						$params,
-						$salesforce_mapping['pull_to_drafts'],
-						false,
-						$salesforce_mapping['wordpress_object_default_status']
-					);
-				} else {
-					$result = $this->wordpress->object_upsert(
-						$salesforce_mapping['wordpress_object'],
-						$upsert_key,
-						$upsert_value,
-						$upsert_methods,
-						$params,
-						$salesforce_mapping['pull_to_drafts'],
-						false
-					);
-				}
+				// we don't need to check the plugin version here because the default status is empty in the mapping class, if there's no value in the database
+				$result = $this->wordpress->object_upsert(
+					$salesforce_mapping['wordpress_object'],
+					$upsert_key,
+					$upsert_value,
+					$upsert_methods,
+					$params,
+					$salesforce_mapping['pull_to_drafts'],
+					false,
+					$salesforce_mapping['wordpress_object_default_status']
+				);
 			} else {
 				// No key or prematch field exists on this field map object, create a new object in WordPress.
 				$op                = 'Create';
@@ -1708,18 +1686,12 @@ class Object_Sync_Sf_Salesforce_Pull {
 						'id' => $mapping_object_id,
 					)
 				);
-				if ( version_compare( $this->version, '1.9.0', '>=' ) ) {
-					$result = $this->wordpress->object_create(
-						$salesforce_mapping['wordpress_object'],
-						$params,
-						$salesforce_mapping['wordpress_object_default_status']
-					);
-				} else {
-					$result = $this->wordpress->object_create(
-						$salesforce_mapping['wordpress_object'],
-						$params
-					);
-				}
+				// we don't need to check the plugin version here because the default status is empty in the mapping class, if there's no value in the database
+				$result = $this->wordpress->object_create(
+					$salesforce_mapping['wordpress_object'],
+					$params,
+					$salesforce_mapping['wordpress_object_default_status']
+				);
 			} // End if().
 		} catch ( WordpressException $e ) {
 			// create log entry for failed create or upsert
