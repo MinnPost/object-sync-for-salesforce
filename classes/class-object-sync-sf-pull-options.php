47e0b2adad5bfe0f9e2eee307decdcfe9982825c
Jonathan Stegall <jstegall@minnpost.com>
2021-09-08 23:08:06 -0500

    upgrade the option keys so nothing breaks with version upgrading

--- classes/class-object-sync-sf-pull-options.php
+++ classes/class-object-sync-sf-pull-options.php
@@ -35,6 +35,13 @@ class Object_Sync_Sf_Pull_Options {
 	public $direction;
 
 	/**
+	 * Option keys that can be upgraded
+	 *
+	 * @var string
+	 */
+	private $upgradeable_keys;
+
+	/**
 	 * Constructor for option records class
 	 */
 	public function __construct() {
@@ -42,6 +49,9 @@ class Object_Sync_Sf_Pull_Options {
 		$this->option_prefix = object_sync_for_salesforce()->option_prefix;
 
 		$this->direction = 'pull';
+
+		$this->upgradeable_keys = $this->get_upgradeable_keys();
+
 	}
 
 	/**
@@ -54,7 +64,14 @@ class Object_Sync_Sf_Pull_Options {
 	private function generate_option_key( $params, $legacy = false ) {
 		array_unshift( $params, substr( $this->option_prefix, 0, -1 ), $this->direction ); // add the prefixes.
 		$params = array_filter( $params, fn( $value ) => ! is_null( $value ) && '' !== $value ); // remove null and empty values.
-		$key    = implode( '_', $params ); // make the key string.
+
+		// legacy keys don't have a fieldmap.
+		if ( true === $legacy && isset( $params['fieldmap_id'] ) ) {
+			unset( $params['fieldmap_id'] );
+		}
+
+		// make the key a string.
+		$key = implode( '_', $params );
 
 		// allow developers to filter the key.
 		$key = apply_filters( $this->option_prefix . 'pull_option_key', $key, $params );
@@ -69,10 +86,11 @@ class Object_Sync_Sf_Pull_Options {
 
 		if ( true === $legacy ) {
 			// allow developers to filter the legacy key.
-			$key = apply_filters( $this->option_prefix . 'pull_option_legacy_key', $params );
+			$key = apply_filters( $this->option_prefix . 'pull_option_legacy_key', $key, $params );
 		}
 
-		$key = esc_attr( $key ); // make sure it's escaped.
+		// note: the WordPress codex indicates that option names do not need to be escaped.
+		// see: https://developer.wordpress.org/reference/functions/update_option/.
 
 		return $key;
 	}
@@ -90,9 +108,9 @@ class Object_Sync_Sf_Pull_Options {
 	public function set( $operation, $object_type = '', $fieldmap_id = '', $value = '', $autoload = true ) {
 		// generate the option key parameters.
 		$params = array(
-			$operation,
-			$object_type,
-			$fieldmap_id,
+			'operation'   => $operation,
+			'object_type' => $object_type,
+			'fieldmap_id' => $fieldmap_id,
 		);
 		$key    = $this->generate_option_key( $params );
 		$value  = isset( $value ) ? $value : '';
@@ -106,6 +124,40 @@ class Object_Sync_Sf_Pull_Options {
 		 */
 
 		$result = update_option( $key, $value, $autoload );
+
+		if ( true === $result ) {
+			$this->legacy_option_upgrade( $operation, $object_type, $fieldmap_id );
+		}
+		return $result;
+	}
+
+	/**
+	 * Set individual option records for sync operations
+	 *
+	 * @param string $operation what is the option related to? last pull, current pull, merge, delete, etc.
+	 * @param string $object_type the Salesforce object type.
+	 * @param int    $fieldmap_id the ID of the specific fieldmap that is running.
+	 * @param mixed  $value the value to be saved in the option.
+	 * @param bool   $autoload whether to autoload the option value.
+	 * @return bool  $result value of the save operation.
+	 */
+	private function legacy_option_upgrade( $operation, $object_type = '', $fieldmap_id = '', $value = '', $autoload = true ) {
+		$result       = false;
+		$legacy_value = $this->legacy_get( $operation, $object_type, $fieldmap_id );
+		if ( false !== $legacy_value ) {
+			// generate the option key parameters.
+			$params = array(
+				'operation'   => $operation,
+				'object_type' => $object_type,
+				'fieldmap_id' => $fieldmap_id,
+			);
+			$key    = $this->generate_option_key( $params, true );
+			$this->add_upgradeable_key( $key );
+			$result = $this->set( $operation, $object_type, $fieldmap_id, $legacy_value );
+			if ( true === $result ) {
+				$this->legacy_delete( $key );
+			}
+		}
 		return $result;
 	}
 
@@ -121,12 +173,11 @@ class Object_Sync_Sf_Pull_Options {
 	public function get( $operation, $object_type = '', $fieldmap_id = '', $default = false ) {
 		// generate the option key parameters.
 		$params = array(
-			$operation,
-			$object_type,
-			$fieldmap_id,
+			'operation'   => $operation,
+			'object_type' => $object_type,
+			'fieldmap_id' => $fieldmap_id,
 		);
 		$key    = $this->generate_option_key( $params );
-		error_log( 'key is ' . $key );
 		$value  = get_option( $key, $default );
 
 		/*
@@ -137,23 +188,10 @@ class Object_Sync_Sf_Pull_Options {
 		 * object_sync_for_salesforce_pull_delete_last_Contact_1
 		 */
 
-		// if the new option does not exist, try to load the old one and save it as a new value.
+		// if the new option value does not exist, try to upgrade the old one.
 		if ( $default === $value ) {
-			error_log( 'load the legacy value' );
-			$legacy_value = $this->legacy_get( $key, $operation, $object_type, $fieldmap_id );
-			if ( false !== $legacy_value ) {
-				error_log( 'there is a legacy value' );
-				$value  = $legacy_value;
-				$result = $this->set( $operation, $object_type, $fieldmap_id, $value );
-				if ( true === $result ) {
-					error_log( 'delete the legacy value' );
-					$this->legacy_delete( $operation, $object_type );
-				}
-			} else {
-				error_log( 'there is not a legacy value' );
-			}
+			$this->legacy_option_upgrade( $operation, $object_type, $fieldmap_id, $value );
 		}
-
 		return $value;
 	}
 
@@ -163,18 +201,18 @@ class Object_Sync_Sf_Pull_Options {
 	 * @param string $operation what is the option related to? last pull, current pull, merge, delete, etc.
 	 * @param string $object_type the Salesforce object type.
 	 * @param int    $fieldmap_id the ID of the specific fieldmap that is running.
+	 * @param mixed  $default the default value for the option.
 	 * @return mixed $value the value of the item. False if it's empty.
 	 */
-	public function legacy_get( $operation, $object_type = '', $fieldmap_id ) {
+	public function legacy_get( $operation, $object_type = '', $fieldmap_id, $default = false ) {
 		// generate the option key parameters.
 		$params = array(
-			$operation,
-			$object_type,
-			$fieldmap_id,
+			'operation'   => $operation,
+			'object_type' => $object_type,
+			'fieldmap_id' => $fieldmap_id,
 		);
 		$key    = $this->generate_option_key( $params, true );
-		$value  = get_option( $key, false );
-
+		$value  = get_option( $key, $default );
 		return $value;
 	}
 
@@ -189,12 +227,11 @@ class Object_Sync_Sf_Pull_Options {
 	public function delete( $operation, $object_type = '', $fieldmap_id = '' ) {
 		// generate the option key parameters.
 		$params = array(
-			$operation,
-			$object_type,
-			$fieldmap_id,
+			'operation'   => $operation,
+			'object_type' => $object_type,
+			'fieldmap_id' => $fieldmap_id,
 		);
 		$key    = $this->generate_option_key( $params );
-
 		$result = delete_option( $key );
 		return $result;
 	}
@@ -202,46 +239,65 @@ class Object_Sync_Sf_Pull_Options {
 	/**
 	 * Delete the legacy individual option records for sync operation
 	 *
-	 * @param string $operation what is the option related to? last pull, current pull, merge, delete, etc.
-	 * @param string $object_type the Salesforce object type.
-	 * @param int    $fieldmap_id the ID of the specific fieldmap that is running.
+	 * @param string $key the legacy key to delete.
 	 * @return bool  $result True if successful, false otherwise.
 	 */
-	public function legacy_delete( $operation, $object_type = '', $fieldmap_id = '' ) {
-		// generate the option key parameters.
-		$params = array(
-			$operation,
-			$object_type,
-			$fieldmap_id,
-		);
-		$key    = $this->generate_option_key( $params, true );
-
+	public function legacy_delete( $key ) {
 		$result = delete_option( $key );
+		if ( true === $result ) {
+			$this->remove_upgradeable_key( $key );
+		}
 		return $result;
 	}
 
 	/**
-	 * Delete the entire set of current sync data
+	 * Add an option key to the array of upgradeable keys.
 	 *
-	 * @return array $result has the success result and how many values were cleared.
+	 * @param string $key the key to add to the array.
+	 * @return array $this->upgradeable_keys the array of keys.
 	 */
-	public function flush() {
-		$keys    = $this->all_keys();
-		$success = true;
-		$count   = 0;
-		if ( ! empty( $keys ) ) {
-			foreach ( $keys as $key ) {
-				$success = delete_transient( $key );
-				$count++;
+	private function add_upgradeable_key( $key ) {
+		$keys   = $this->get_upgradeable_keys();
+		$keys[] = $key;
+		$result = update_option( $this->option_prefix . 'upgradeable_keys', $keys );
+		if ( true === $result ) {
+			$this->upgradeable_keys = $keys;
+			return $this->upgradeable_keys;
+		}
+	}
+
+	/**
+	 * Remove an option key from the array of upgradeable keys.
+	 *
+	 * @param string $key the key to remove from the array.
+	 * @return array $this->upgradeable_keys the array of keys.
+	 */
+	private function remove_upgradeable_key( $key ) {
+		$keys      = $this->get_upgradeable_keys();
+		$array_key = array_search( $key, $keys, true );
+		if ( false !== $array_key ) {
+			unset( $keys[ $array_key ] );
+		}
+		error_log( 'now the keys: ' . print_r( $keys, true ) );
+		$result = update_option( $this->option_prefix . 'upgradeable_keys', $keys );
+		if ( true === $result ) {
+			$this->upgradeable_keys = $keys;
+			if ( empty( $keys ) ) {
+				delete_option( $this->option_prefix . 'upgradeable_keys' );
 			}
+			return $this->upgradeable_keys;
 		}
-		$success = delete_transient( $this->name );
-		if ( true === $success ) {
-			$count++;
-		}
-		$result            = array();
-		$result['success'] = $success;
-		$result['count']   = $count;
-		return $result;
+	}
+
+	/**
+	 * Get the array of upgradeable keys.
+	 *
+	 * @return array $this->upgradeable_keys the array of keys.
+	 */
+	private function get_upgradeable_keys() {
+		$keys                   = get_option( $this->option_prefix . 'upgradeable_keys', array() );
+		$keys                   = array_unique( $keys );
+		$this->upgradeable_keys = $keys;
+		return $this->upgradeable_keys;
 	}
 }
