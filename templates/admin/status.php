1048bfe469073da4b9cc42178a5eb56d8245838c
Jonathan Stegall <jstegall@minnpost.com>
2022-05-10 10:09:18 -0500

    explain where the active api version is coming from

--- templates/admin/status.php
+++ templates/admin/status.php
@@ -7,21 +7,26 @@
 
 ?>
 
-<h3><?php echo esc_html__( 'Salesforce Status', 'object-sync-for-salesforce' ); ?></h3>
+<h3><?php echo esc_html__( 'Salesforce API Connection Status', 'object-sync-for-salesforce' ); ?></h3>
 
-<?php if ( false === $this->login_credentials['using_deprecated_option'] && false === $this->login_credentials['using_developer_filter'] ) : ?>
-	<p>
+<p>
 	<?php
-		echo sprintf(
-			// translators: placeholder is for the version number of the Salesforce REST API.
-			esc_html__( 'The plugin is using version %1$s of the Salesforce REST API. This is the recommended configuration.', 'object-sync-for-salesforce' ),
-			esc_html( $this->login_credentials['rest_api_version'] )
-		);
+	echo sprintf(
+		// translators: placeholder is for the version number of the Salesforce REST API.
+		esc_html__( 'Object Sync for Salesforce is making calls to the Salesforce REST API using version %1$s. ', 'object-sync-for-salesforce' ),
+		esc_html( $this->login_credentials['rest_api_version'] )
+	);
 	?>
-	</p>
-<?php else : ?>
-	<?php echo esc_html( $this->login_credentials['rest_api_version'] ); ?>
-<?php endif; ?>
+	<?php if ( false === $this->login_credentials['using_deprecated_option'] && false === $this->login_credentials['using_developer_filter'] ) : ?>
+		<?php
+			echo esc_html__( 'This plugin works to keep up to date with the release cycle of API versions from Salesforce. When a new version is released, the plugin will upgrade to that version as soon as possible. If you upgrade the plugin when new releases come out, you will always be on the highest supported version of the Salesforce REST API. Object Sync for Salesforce will not include plugin functionality that depends on a version it does not support, and will ensure that all releases follow this pattern.', 'object-sync-for-salesforce' );
+		?>
+	<?php elseif ( true === $this->login_credentials['using_deprecated_option'] ) : ?>
+		<?php echo esc_html__( 'This API version is set from a previous version of the plugin, and the plugin functionality of choosing the API version in the interface has been deprecated. In a future release (likely version 3.0.0), the ability to use the REST API version set by the interface will be removed.', 'object-sync-for-salesforce' ); ?>
+	<?php else : ?>
+		<?php echo esc_html__( 'This API version is set with the use of a developer hook.', 'object-sync-for-salesforce' ); ?>
+	<?php endif; ?>
+</p>
 
 <h3><?php echo esc_html__( 'Test Salesforce API Call', 'object-sync-for-salesforce' ); ?></h3>
 <?php if ( '' !== $contacts_apicall_summary ) : ?>
@@ -31,8 +36,8 @@
 				<h4><?php echo $contacts_apicall_summary; ?></h4>
 			</summary>
 			<tr>
-				<th><?php echo esc_html__( 'Contact ID' ); ?></th>
-				<th><?php echo esc_html__( 'Name' ); ?></th>
+				<th><?php echo esc_html__( 'Contact ID', 'object-sync-for-salesforce' ); ?></th>
+				<th><?php echo esc_html__( 'Name', 'object-sync-for-salesforce' ); ?></th>
 			</tr>
 		</thead>
 		<tbody>
